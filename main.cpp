f2fe7ccf649511ca98857f590b09259837146b22
Aditya <mr.adityagupta11@gmail.com>
2025-11-19 15:14:11 +0530

    updated code

--- main.cpp
+++ main.cpp
@@ -150,14 +150,55 @@
 // 5? Armstrong Number
 // 5. Armstrong Number
 
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// #include<math.h>
+// using namespace std;
+// int countdigit(int n)
+// {
+//     int count=0;
+//     while(n)
+//     {
+//         count++;
+//         n/=10;
+//     }
+//     return count;
+// }
+// bool Armstrong(int num,int digit)
+// {
+//     int n=num,ans=0,rem;
+//     while(n)
+//     {
+//         rem=n%10;
+//         n/=10;
+//         ans=ans+ pow(rem,digit);
+//     }
+//     if(ans==num)
+//     {
+//         return 1;
+        
+//     }
+//     else
+//     {
+//         return 0;
+//     }
+// }
+// int main()
+// {
+//     int num;
+//     cin>>num;
+//     int digit=countdigit(num);
+//     cout<<Armstrong(num,digit);
 
+// }
 
-// 6?
-// 6.
+// 6? Find Trailing Zero In A Factorial Or Bishop Or Nim Game
+// 6. Find Trailing Zero In A Factorial Or Bishop Or Nim Game
+
 // 7?
 // 7.
+
+
+
 // 8?
 // 8.
 // 9?
